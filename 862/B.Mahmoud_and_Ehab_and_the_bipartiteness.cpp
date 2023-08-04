8623001c7f4bbc0ab3e3521795343666967d62b7
sahadat-11 <137852614+sahadat-11@users.noreply.github.com>
2023-08-04 15:40:00 +0600

    added

--- 862/B.Mahmoud_and_Ehab_and_the_bipartiteness.cpp
+++ 862/B.Mahmoud_and_Ehab_and_the_bipartiteness.cpp
@@ -35,3 +35,41 @@ int main() {
   return 0;
 }
 
+
+//In The Name of ALLAH
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N = 1e5 + 7, inf = 1e9;
+vector<int> adj[N];
+int main() {
+  int n; cin >> n;
+  for(int i = 1; i < n; i++) {
+      int u, v; cin >> u >> v;
+      adj[u].push_back(v);
+      adj[v].push_back(u);
+   }
+    vector<int> dis(n + 1, inf);
+    queue<int> q;
+    q.push(1);
+    dis[1] = 0;
+    while(!q.empty()) {
+      int u = q.front();
+      q.pop();
+      for(auto v : adj[u]) {
+        if(dis[u] + 1 < dis[v]) {
+          dis[v] = dis[u] + 1;
+          q.push(v);
+        }
+      }
+    }
+    int black = 0, white = 0;
+    for(int i = 1; i <= n; i++) {
+      if(dis[i] % 2) black++;
+      else white++;
+    }
+    //cout << black << " " << white << "\n";
+    cout << 1ll*black * white - n + 1;
+  return 0;
+}
+
