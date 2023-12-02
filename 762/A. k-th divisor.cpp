7c7c7a793f7ffa8859bad9c58bf0e34897c147ac
sahadat-11 <137852614+sahadat-11@users.noreply.github.com>
2023-12-02 20:23:42 +0600

    added

--- 762/A. k-th divisor.cpp
+++ 762/A. k-th divisor.cpp
@@ -10,11 +10,13 @@ int main() {
    for(ll i = 1; i * i <= n; i++) {
       if(n % i == 0) {
          divs.push_back(i);
-         if(i != n / i) divs.push_back(n / i);
+         if(i != n / i) {
+            divs.push_back(n / i);
+         }
       }
    }
    sort(divs.begin(), divs.end());
-   if(divs.size() < k) cout << - 1 << "\n";
-   else cout << divs[k - 1] << "\n";
-   return 0;
+   if(k > divs.size()) cout << -1 << "\n";
+   else cout << divs[k - 1];
+   return 0; 
 }
